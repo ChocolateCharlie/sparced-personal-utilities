bbe2e7308a4bbcafe1492cfe4044554b528304bd
ChocolateCharlie <aurore.amrit@gmail.com>
2022-07-15 17:50:32 -0400

    [sparced_pipeline] Create sbml_utils module for SBML annotations

--- sparced_pipeline/bin/__init__.py
+++ sparced_pipeline/bin/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Sparced Pipeline Modules"""
-from bin.copydir import copy_directory
 from bin.antimony_utils import *
+from bin.copydir import copy_directory
+from bin.sbml_utils import *
