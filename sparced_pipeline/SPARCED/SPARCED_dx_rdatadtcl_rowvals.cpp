a0d12c0f4787a51486cf376632e1cbf8076cbafc
ChocolateCharlie <aurore.amrit@gmail.com>
2022-07-15 18:15:24 -0400

    Model generated with new code

---
+++ sparced_pipeline/SPARCED/SPARCED_dx_rdatadtcl_rowvals.cpp
@@ -0,0 +1,18 @@
+#include "amici/sundials_matrix_wrapper.h"
+#include "sundials/sundials_types.h"
+
+#include <array>
+#include <algorithm>
+
+namespace amici {
+namespace model_SPARCED {
+
+static constexpr std::array<sunindextype, 141> dx_rdatadtcl_rowvals_SPARCED_ = {
+    913, 912, 911, 910, 909, 908, 907, 906, 905, 904, 903, 902, 901, 900, 899, 898, 897, 896, 895, 894, 893, 892, 891, 890, 889, 888, 887, 886, 885, 884, 883, 882, 881, 880, 879, 878, 877, 876, 875, 874, 873, 872, 871, 870, 869, 868, 867, 866, 865, 864, 863, 862, 861, 860, 859, 858, 857, 856, 855, 854, 853, 852, 851, 850, 849, 848, 847, 846, 845, 844, 843, 842, 841, 840, 839, 838, 837, 836, 835, 834, 833, 832, 831, 830, 829, 828, 827, 826, 825, 824, 823, 822, 821, 820, 819, 818, 817, 816, 815, 814, 813, 812, 811, 810, 809, 808, 807, 806, 805, 804, 803, 802, 801, 800, 799, 798, 797, 796, 795, 794, 793, 792, 791, 790, 789, 788, 787, 786, 785, 784, 783, 782, 781, 780, 779, 778, 777, 776, 775, 774, 773
+};
+
+void dx_rdatadtcl_rowvals_SPARCED(SUNMatrixWrapper &dx_rdatadtcl){
+    dx_rdatadtcl.set_indexvals(gsl::make_span(dx_rdatadtcl_rowvals_SPARCED_));
+}
+} // namespace model_SPARCED
+} // namespace amici
